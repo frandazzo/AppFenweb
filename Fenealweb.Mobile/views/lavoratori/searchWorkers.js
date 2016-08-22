b5fff559a36fa301d599971bc7f997d7d8323313
Francesco Randazzo <fg.randazzo@hotmail.it>
2016-08-22 14:00:33 +0200

    q

--- Fenealweb.Mobile/views/lavoratori/searchWorkers.js
+++ Fenealweb.Mobile/views/lavoratori/searchWorkers.js
@@ -1,8 +1,23 @@
 ﻿Fenealweb.searchWorkers = function (params) {
     "use strict";
 
+    var cognome = ko.observable('');
+    var nome = ko.observable('');
+    var fiscale = ko.observable('');
+
     var viewModel = {
-//  Put the binding properties here
+        search: function () {
+            alert('cognome: ' + cognome() + ' nome: ' + nome() + ' fiscale: ' + fiscale() );
+        },
+        cognomeOptions:{
+            value: cognome, mode: 'search', showClearButton: true
+        },
+        nomeOptions: {
+            value : nome, mode: 'search', showClearButton: true
+        },
+        fiscaleOptions: {
+            value: fiscale, mode: 'search', showClearButton: true
+        },
     };
 
     return viewModel;
